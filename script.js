c1f4a6ead990652add0799a062a3c702110fcfb8
Alexander Grattan <alex@grattan.me>
2020-09-20 19:00:28 -0400

    Preloader and fast animation

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let menu = document.getElementById("menu");
 let nav = document.getElementById("nav");
 let exit = document.getElementById("exit");
 
+window.addEventListener("load", () => {
+  const preload = document.querySelector(".preload");
+  preload.classList.add("preload-finish");
+});
+
 menu.addEventListener("click", function (e) {
   nav.classList.toggle("hide-mobile");
   e.preventDefault();
